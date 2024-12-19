e7dd63a906607ecf6ad9c0e96d69449dc6500f9a
Muhammadreza Haghiri <haghiri75@gmail.com>
2024-12-20 00:51:19 +0330

    wip

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from config import *
 client = OpenAI(api_key = OPENAI_API_KEY, base_url = OPENAI_ENDPOINT)
 
 def improver(prompt):
-    pass
+    
+    client.chat.completions.create()
 
 if __name__ == "__main__":
     while True:
